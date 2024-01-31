e2b6e130803bd618f42378a7a93cb01f643a926a
Nitika Jain <nitikaaajain.04@gmail.com>
2024-01-31 20:37:17 +0530

    commits

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '34aebc15-bd99-439c-86c1-440278b509fb';
+export const MODAL_CLOSE_SEC = 2.5;
